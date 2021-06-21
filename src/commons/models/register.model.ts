6836b937d5df6806dc08d2cf3a4957625905a273
Victor Belchior <victor@thau.tech>
2021-06-21 20:14:05 -0300

    Implementa Modelo e servico de Registro

---
+++ src/commons/models/register.model.ts
@@ -0,0 +1,30 @@
+import { TypeUtil } from '../utils';
+
+export class RegisterModel {
+	public id: number;
+
+	public date: Date;
+
+	public dateEnd: Date;
+
+	public status: number;
+
+	public risk: number;
+
+	public priority: number;
+
+	public isOnline: boolean;
+
+	constructor(json?: any) {
+		if (json != undefined && json != null) {
+			const keys: Array<string> = Object.keys(json);
+			if (keys.includes('id')) this.id = json.id ? Number(json.id) : json.id;
+			if (keys.includes('date')) this.date = json.date ? new Date(json.date) : json.date;
+			if (keys.includes('dateEnd')) this.dateEnd = json.dateEnd ? new Date(json.dateEnd) : json.dateEnd;
+			if (keys.includes('status')) this.status = json.status ? Number(json.staatus) : json.status;
+			if (keys.includes('risk')) this.risk = json.risk ? Number(json.risk) : json.risk;
+			if (keys.includes('priority')) this.priority = json.priority ? Number(json.priority) : json.priority;
+			if (keys.includes('isOnline')) this.isOnline = json.isOnline ? json.isOnline : json.isOnline;
+		}
+	}
+}
---
+++ src/commons/services/register.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { RegisterModel } from '../models';
+import { TypeUtil } from '../utils';
+import { environment } from 'src/environments/environment';
+
+interface CountInterface {
+	count: number;
+}
+
+@Injectable({
+	providedIn: 'root'
+})
+export class RegisterService {
+	constructor(private httpClient: HttpClient) {}
+
+	private static readonly NULL_ID: string = 'Entity id is null or undefined.';
+
+	public create(register: RegisterModel): Observable<RegisterModel> {
+		const path: string = `${environment.server}/registers`;
+		const json: string = JSON.stringify({ register });
+		return this.httpClient.post<RegisterModel>(path, register).pipe(catchError((error) => throwError(error)));
+	}
+
+	public retrieve(register: RegisterModel): Observable<RegisterModel> {
+		if (!TypeUtil.exists(register.id)) return of(new RegisterModel());
+		const path: string = `${environment.server}/registers/${register.id}`;
+		return this.httpClient.get<RegisterModel>(path).pipe(catchError((error) => throwError(error)));
+	}
+
+	public replace(current: RegisterModel, replacement: RegisterModel, cascade?: boolean): Observable<Object> {
+		if (!TypeUtil.exists(current.id)) return throwError(new Error(RegisterService.NULL_ID));
+		const path: string = `${environment.server}/registers/${current.id}`;
+		const json: string = JSON.stringify({ replacement });
+		let query: HttpParams = new HttpParams();
+		if (TypeUtil.exists(cascade)) query.append('cascade', String(cascade));
+		return this.httpClient.put(path, replacement, { params: query }).pipe(catchError((error) => throwError(error)));
+	}
+
+	public update(current: RegisterModel, replacement: RegisterModel, cascade?: boolean): Observable<Object> {
+		if (!TypeUtil.exists(current.id)) return throwError(new Error(RegisterService.NULL_ID));
+		const path: string = `${environment.server}/registers/${current.id}`;
+		const json: string = JSON.stringify({ replacement });
+		let query: HttpParams = new HttpParams();
+		if (TypeUtil.exists(cascade)) query.append('cascade', String(cascade));
+		return this.httpClient.patch(path, replacement, { params: query }).pipe(catchError((error) => throwError(error)));
+	}
+
+	public delete(register: RegisterModel): Observable<Object> {
+		const path: string = `${environment.server}/registers/${register.id}`;
+		return this.httpClient.delete(path).pipe(catchError((error) => throwError(error)));
+	}
+
+	/*public filter(
+        zona?: number
+    ): Observable<RegisterModel[]> {
+        const path: string = `${environment.server}/escolas`;
+        let query: HttpParams = new HttpParams();
+        if (TypeUtil.isFullString(municipio, true)) query = query.append('municipioParte', municipio);
+        if (TypeUtil.exists(zona)) query = query.append('idZona', String(zona));
+
+    }*/
+
+	public count(): Observable<number> {
+		let query: HttpParams = new HttpParams();
+
+		const path: string = `${environment.server}/registers/count`;
+		const subject = new BehaviorSubject<number>(-1); // no count yet
+		this.httpClient
+			.get<number>(path, { params: query })
+			.pipe(catchError((error) => throwError(error)))
+			.subscribe((response: number) => {
+				subject.next(response);
+			});
+		return subject.asObservable();
+	}
+}
